05cc1c513a16261b34d5f430e5af6b712389c2c9
cpelite <cpelite@zlatina.tech>
2024-01-17 21:00:07 +0100

    added functions to create files, and to append existing files.

--- src/tools/fileops.rs
+++ src/tools/fileops.rs
@@ -1,6 +1,7 @@
-use std::io;
-use std::fs::File;
+use std::io::{self, Write};
+use std::fs::{File, OpenOptions};
 use std::io::prelude;
+use std::path::Path;
 
 use crate::main;
 use crate::initandmain::return_to_nm;
@@ -28,18 +29,78 @@ fn fops_main() {
 
     if fopsusrinput.trim() == "help" {
         fops_help();
+
     } else if fopsusrinput.trim() == "?" {
         fops_help();
+    }
+
+    else if fopsusrinput.trim() == "mkfile" {
+        fops_mkfile();
     }
 
     else if fopsusrinput.trim() == "nm" {
         return_to_nm();
     }
+
+    else if fopsusrinput.trim() == "fileappend" {
+        fops_fileappend();
+    }
 }
+
+fn fops_mkfile() {
+    let mut user_input = String::new();
+    println!("Enter the file path: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut user_input).expect("Failed to read line!");
+    let file_path = user_input.trim();
+
+    match File::create(file_path) {
+        Ok(mut file) => {
+            let content = "File successfully created!";
+            match file.write_all(content.as_bytes()) {
+                Ok(_) => println!("File created successfully at: {}", file_path),
+                Err(e) => eprintln!("Failed to write content to file: {}", e),
+            }
+        }
+
+        Err(e) => eprintln!("Failed to create file: {}", e),
+    }
+    fops_main()
+}
+
+fn fops_fileappend() {
+    let mut user_input = String::new();
+    println!("Enter the file path: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut user_input).expect("Failed to read line!");
+    let file_path = user_input.trim();
+
+    let mut append_content = String::new();
+    println!("Please enter what should be appended to file: ");
+    io::stdin().read_line(&mut append_content);
+
+    match OpenOptions::new().append(true).create(true).open(file_path) {
+        Ok(mut file) => {
+            let content = &append_content;
+            match file.write_all(content.as_bytes()) {
+                Ok(_) => println!("Content appended successfully to: {}", file_path),
+                Err(e) => eprintln!("Failed to write content to file: {}", e),
+            }
+        }
+
+        Err(e) => eprintln!("Failed to open or create file: {}", e),
+    }
+    fops_main();
+}
+
+
+
+
 
 fn fops_help() {
     println!("The following commands are available in FOPS mode:");
     println!("help / ? - displays this command listing.");
     println!("nm - returns to normal mode.");
+    println!("mkfile - creates a new file.");
     fops_main()
 }
